40c369c54dce28471935b3ffdc41aa6357d8f4c5
root <root@lucas.mitsuo>
2021-06-09 17:27:00 -0300

    hello world

--- src/main/java/br/com/tnttec/TntReportApplication.java
+++ src/main/java/br/com/tnttec/TntReportApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @RestController
-public class TntReportApplication {
+public class TntReportApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TntReportApplication.class, args);
